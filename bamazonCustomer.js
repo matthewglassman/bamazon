474220221885ca238efe1d2658ee746c9ad74c47
matthewglassman <matthewglassmn78@gmail.com>
2017-02-27 21:05:21 -0500

    Adding conditionals for handling inquire module choices. Need to add handling for user requesting a quantity greater than what is in stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
 
 	user: "root",
 	password: "",
-	database: "bamazonproducts"
+	database: "Bamazon"
 });
 
 //test connection
@@ -20,4 +20,67 @@ connection.connect(function(err){
 connection.query("SELECT * FROM products", function(err, res){
 	if (err) throw err;
 	console.log(res);
+
+connection.end();
 });
+
+//Begin Actual Program
+var consumerStart = function(){
+	connection.query("SELECT * FROM products", function(err, products){
+		if (err) throw err;
+		console.log(products);
+	});
+
+	//Run inquirer module to pose two questions to the the Buy or Leave
+	inquirer.prompt({
+		name:"BuyOrLeave",
+		type: "rawlist",
+		message: "Would you like to [Buy Something] or [Leave]?",
+		choices: ["Buy Something", "Leave"]
+	}).then(function(answer){
+		if (answer.BuyOrLeave.toLowerCase() === "leave"){
+			leaveStore();
+		}
+		else{
+			goShopping();
+		}
+	});
+};
+
+var leaveStore = function(){
+	console.log("Thanks for stopping by. Come again soon!");
+	connection.end();
+};
+
+var goShopping = function(){
+	//User inquirer to ask two questions. 1. What is the ID of hte product to buy and 2. How many units would they like to buy.
+	connection.query("SELECT * FROM products", function(err, products){
+		console.log(products);
+		inquirer.prompt({
+			name: "items"
+			type: "rawlist"
+			choices: function(value){
+				var productArray = [];
+				for (var i = 0; i < products.length; i++){
+					productArray.push(products[i].item_id),
+								.push(products[i].product_name),
+								.push(products[i].price),
+								.push(products[i].stock_quantity);
+				}
+				return productArray;
+			},
+			message: "What product would you like to buy?"
+		}).then(function(product){
+			for (var i = 0; i < products.length; i++){
+				if (products[i].item_id === product.choice){
+					var chosenProduct = products[i];
+					inquirer.prompt({
+						name: "quantity",
+						type: "input",
+						message: "How many would you like?"
+					}).then(function())  //build what should happen if amount of item requested is not in stock and what should happen if it is.
+				}
+			}
+		})
+	}
+}
