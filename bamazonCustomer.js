e7ec91636e04d3ad323ba97592ec7581169f0bf3
matthewglassman <matthewglassmn78@gmail.com>
2017-03-01 14:43:29 -0500

    Code working now.  Need to clean it up a bit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+require("console.table");
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -11,24 +11,24 @@ var connection = mysql.createConnection({
 });
 
 //test connection
-connection.connect(function(err){
-	if (err) throw err;
-	console.log("Connected as id " + connection.threadId);
-});
+// connection.connect(function(err){
+// 	if (err) throw err;
+// 	console.log("Connected as id " + connection.threadId);
+// });
 
-//test selecting and showing all data from querying Products table
-connection.query("SELECT * FROM products", function(err, res){
-	if (err) throw err;
-	console.log(res);
+// //test selecting and showing all data from querying Products table
+// connection.query("SELECT * FROM products", function(err, res){
+// 	if (err) throw err;
+// 	console.log(res);
 
-connection.end();
-});
+// connection.end();
+// });
 
 //Begin Actual Program
 var consumerStart = function(){
 	connection.query("SELECT * FROM products", function(err, products){
 		if (err) throw err;
-		console.log(products);
+		console.table(products);
 	});
 
 	//Run inquirer module to pose two questions to the the Buy or Leave
@@ -55,24 +55,21 @@ var leaveStore = function(){
 var goShopping = function(){
 	//User inquirer to ask two questions. 1. What is the ID of hte product to buy and 2. How many units would they like to buy.
 	connection.query("SELECT * FROM products", function(err, products){
-		console.log(products);
+		console.table(products);
 		inquirer.prompt({
-			name: "items"
-			type: "rawlist"
-			choices: function(value){
-				var productArray = [];
-				for (var i = 0; i < products.length; i++){
-					productArray.push(products[i].item_id),
-								.push(products[i].product_name),
-								.push(products[i].price),
-								.push(products[i].stock_quantity);
-				}
-				return productArray;
-			},
+			name: "items",
+			type: "input",
+			// choices: function(value){
+			// 	var productArray = [];
+			// 	for (var i = 0; i < products.length; i++){
+			// 		productArray.push(products[i].item_id, products[i].product_name, products[i].price, products[i].stock_quantity])					
+			// 	}
+			// 	return productArray;
+			// },
 			message: "What product would you like to buy?"
 		}).then(function(product){
 			for (var i = 0; i < products.length; i++){
-				if (products[i].item_id === product.choice){
+				if (products[i].item_id == product.items){
 					var chosenProduct = products[i];
 					inquirer.prompt({
 						name: "quantity",
@@ -88,19 +85,22 @@ var goShopping = function(){
 							//Do the math to display item price, requested amount and total amount (price * amount).
 							var totalPrice = chosenProduct.price * item.quantity;
 							var newQuantity = chosenProduct.stock_quantity - item.quantity;
+							console.log("New Quantity " + newQuantity);
 							connection.query("UPDATE products SET ? WHERE ?", [{
 								stock_quantity: newQuantity
 							}, {
-								item_id: product.choice
+								item_id: product.items
 							}], function(err, res){
-								console.log("Thank you for purchasing " + item.quantity+ " " + products[i].product_name + "'s.  Your total is " + totalPrice + ".")
+								console.log("Thank you for purchasing " + item.quantity + " " + chosenProduct.product_name + "'s.  Your total is " + totalPrice + ".")
 								consumerStart();
 							});
 
 						}
 					});
 				}
+				console.log(product.items, products[i].item_id);
 			}
 		});
-	}
+	});
 }
+consumerStart();
